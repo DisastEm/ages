e0659a88dd66291dd35afc293297c8e3d84cc445
nikita <nik@em.com>
2021-01-22 14:00:59 +0500

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+function name() {
+    var a = prompt('Введите имя');
+    var b = prompt('Какого вы года?');
+    var c = prompt('Нынешний год?');
+    alert ('Имя ' + a + ', год рождения ' + b + ', нынешний год ' + c )
+    console.log(c - b);
+}
+name ()
